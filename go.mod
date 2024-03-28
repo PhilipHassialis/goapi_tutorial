0b192d4a365b9e0306bdc26f7b37f54726aad612
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2024-03-28 09:48:41 +0200

    Completed Tutorial Commit

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/PhilipHassialis/goapi_tutorial
 
 go 1.21.6
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/gorilla/schema v1.3.0
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require golang.org/x/sys v0.18.0 // indirect
---
+++ internal/handlers/tools/mockdb.go
@@ -0,0 +1,59 @@
+package tools
+
+import "time"
+
+type mockDB struct{}
+
+var mockLoginDetails = map[string]LoginDetails{
+	"alex": {
+		AuthToken: "123ABC",
+		Username:  "alex",
+	},
+	"jason": {
+		AuthToken: "456DEF",
+		Username:  "jason",
+	},
+	"maria": {
+		AuthToken: "789GHI",
+		Username:  "maria",
+	},
+}
+
+var mockCoinDetails = map[string]CoinDetails{
+	"alex": {
+		Coins:    100,
+		Username: "alex",
+	},
+	"jason": {
+		Coins:    150,
+		Username: "jason",
+	},
+	"maria": {
+		Coins:    200,
+		Username: "maria",
+	},
+}
+
+func (d *mockDB) GetUserLoginDetails(username string) *LoginDetails {
+	time.Sleep(time.Second * 2)
+	var clientData = LoginDetails{}
+	clientData, ok := mockLoginDetails[username]
+	if !ok {
+		return nil
+	}
+	return &clientData
+}
+
+func (d *mockDB) GetUserCoins(username string) *CoinDetails {
+	time.Sleep(time.Second * 1)
+	var clientData = CoinDetails{}
+	clientData, ok := mockCoinDetails[username]
+	if !ok {
+		return nil
+	}
+	return &clientData
+}
+
+func (d *mockDB) SetupDatabase() error {
+	return nil
+}
